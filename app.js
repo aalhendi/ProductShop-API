70526444ddc719b89c54e1edfea46b754c5f0a7f
aalhendi <abdulrazaq.alhendi@gmail.com>
2021-06-29 18:41:37 +0300

    Add delete product route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// Imports
 const express = require("express");
-const products = require("./products");
+let products = require("./products");
 const cors = require("cors");
 
 const app = express();
@@ -7,10 +8,46 @@ const app = express();
 //Middleware
 app.use(cors());
 
-//Routes
+//========== Product Routes ==========
+
+// Products List
 app.get("/products", (req, res) => {
   res.json(products);
 });
+
+// Find Product
+app.get("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  try {
+    const foundProduct = products.find((product) => +productId === product.id);
+    if (foundProduct) {
+      res.json(foundProduct);
+    } else {
+      res.status(404).json({
+        message: "Product does not exist",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Delete Product
+app.delete("/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  try {
+    const foundProduct = products.find((product) => +productId === product.id);
+    if (foundProduct) {
+      products = products.filter((product) => +productId !== product.id);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+//========== End Product Routes ==========
 
 //Listen @ port 8000
 app.listen(8000, () => {
